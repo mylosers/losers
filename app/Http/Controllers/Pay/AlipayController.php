c039ae7c58f1772fb0ae24653995077ebe615f44
mylosers <3092256637@qq.com>
2019-01-14 09:36:59 +0800

    支付

--- app/Http/Controllers/Pay/AlipayController.php
+++ app/Http/Controllers/Pay/AlipayController.php
@@ -15,7 +15,7 @@ class AlipayController extends Controller
     public $app_id = '2016091900549936';
     public $gate_way = 'https://openapi.alipaydev.com/gateway.do';
     public $notify_url = 'http://laravel.myloser.club/pay/alipay/notify';
-    public $rsaPrivateKeyFilePath = './key/priv.key';
+    public $rsaPrivateKeyFilePath = '/key/priv.key';
 
 
     /**
