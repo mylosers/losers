033a54be1bd7c11372c12cfca2754034ab7983a2
mylosers <3092256637@qq.com>
2019-02-19 16:07:49 +0800

    关注回复

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -47,11 +47,13 @@ class WeixinController extends Controller
         //var_dump($xml);echo '<hr>';
         $openid = $xml->FromUserName;               //用户openid
 
+        // 处理用户发送消息
         if(isset($xml->MsgType)){
-            if($xml->MsgType=="text"){
-                $msg=$xml->Content;
-                $xml_response = '<xml><ToUserName><![CDATA['.$openid.']]></ToUserName><FromUserName><![CDATA['.$xml->ToUserName.']]></FromUserName><CreateTime>'.time().'</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[".$msg.", 现在时间'. date('Y-m-d H:i:s') .']]></Content></xml>';
-                echo $xml_response;exit;
+            if($xml->MsgType=='text'){            //用户发送文本消息
+                $msg = $xml->Content;
+                $xml_response = '<xml><ToUserName><![CDATA['.$openid.']]></ToUserName><FromUserName><![CDATA['.$xml->ToUserName.']]></FromUserName><CreateTime>'.time().'</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA['. $msg. date('Y-m-d H:i:s') .']]></Content></xml>';
+                echo $xml_response;
+                exit();
             }
         }
 
