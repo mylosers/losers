f19da55a679d6abaa56c19cda525a5ff3e879675
mylosers <3092256637@qq.com>
2019-02-19 09:17:37 +0800

    服务号菜单

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 use Illuminate\Support\Facades\Redis;
+use GuzzleHttp;
 
 class WeixinController extends Controller
 {
@@ -17,8 +18,7 @@ class WeixinController extends Controller
     public function test()
     {
         //echo __METHOD__;
-        //$this->getWXAccessToken();
-        $this->getUserInfo(1);
+        echo 'Token: '. $this->getWXAccessToken();
     }
 
     /**
@@ -127,13 +127,58 @@ class WeixinController extends Controller
     public function getUserInfo($openid)
     {
         //$openid = 'oLreB1jAnJFzV_8AGWUZlfuaoQto';
-        $access_token = $this->getWXAccessToken();
+        $access_token = $this->getWXAccessToken();      //请求每一个接口必须有 access_token
         $url = 'https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
 
         $data = json_decode(file_get_contents($url),true);
         //echo '<pre>';print_r($data);echo '</pre>';
         return $data;
     }
+
+    /**
+     * 创建服务号菜单
+     */
+    public function createMenu(){
+        //echo __METHOD__;
+        // 1 获取access_token 拼接请求接口
+        $url = 'https://api.weixin.qq.com/cgi-bin/menu/create?access_token='.$this->getWXAccessToken();
+        //echo $url;echo '</br>';
+
+        //2 请求微信接口
+        $client = new GuzzleHttp\Client(['base_uri' => $url]);
+
+        $data = [
+            "button"    => [
+                [
+                    "type"  => "view",      // view类型 跳转指定 URL
+                    "name"  => "Lening222",
+                    "url"   => "https://www.baidu.com"
+                ]
+            ]
+        ];
+
+
+        $r = $client->request('POST', $url, [
+            'body' => json_encode($data)
+        ]);
+
+        // 3 解析微信接口返回信息
+
+        $response_arr = json_decode($r->getBody(),true);
+        //echo '<pre>';print_r($response_arr);echo '</pre>';
+
+        if($response_arr['errcode'] == 0){
+            echo "菜单创建成功";
+        }else{
+            echo "菜单创建失败，请重试";echo '</br>';
+            echo $response_arr['errmsg'];
+
+        }
+
+
+
+    }
+
 
 
 
