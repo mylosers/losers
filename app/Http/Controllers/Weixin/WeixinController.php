4e55ed8f4d78ec3e23dd4285f1d7c3ae0c19b819
mylosers <3092256637@qq.com>
2019-02-19 11:12:19 +0800

    二级菜单

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -151,8 +151,16 @@ class WeixinController extends Controller
             "button"    => [
                 [
                     "type"  => "view",      // view类型 跳转指定 URL
-                    "name"  => "Lening222",
+                    "name"  => "baidu",
                     "url"   => "https://www.baidu.com"
+                ],
+                [
+                    "name" => "caidan",
+                    "sub_button" => [
+                        "type" => "view",
+                        "name" => "sousuo",
+                        "url" => "http://www.soso.com/"
+                    ]
                 ]
             ]
         ];
