ed01b16077c5c5aea70c3279ca01ca7a9dc30609
mylosers <3092256637@qq.com>
2019-02-28 14:43:43 +0800

    微信支付

--- resources/views/goods/order.blade.php
+++ resources/views/goods/order.blade.php
@@ -7,7 +7,8 @@
             @foreach($list as $k=>$v)
             <tr>
                 <td>订单ID: {{$v['order_sn']}} --  订单总价：¥{{$v['order_amount']}}   --  下单时间：{{date('Y-m-d H:i:s',$v['add_time'])}}</td>
-                <td><a href="/pay/{{$v['oid']}}" class="btn btn-info">去支付</a></td>
+                <td><a href="/pay/{{$v['oid']}}" class="btn btn-info">支付宝支付</a></td>
+                <td><a href="/weixin/pay/test/{{$v['oid']}}" class="btn btn-info">微信支付</a></td>
             </tr>
             @endforeach
         </table>
